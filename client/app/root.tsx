bf0a362c97ac60154d71445346fc811547b23e39
Alec Caputo <aleccaputo7@gmail.com>
2024-07-24 18:11:42 -0400

    still can't figure out why theme isn't working

--- client/app/root.tsx
+++ client/app/root.tsx
@@ -5,7 +5,12 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import "./tailwind.css";
+import {LinksFunction} from "@remix-run/node";
+import styles from "~/tailwind.css?url";
+
+export const links: LinksFunction = () => [
+    { rel: "stylesheet", href: styles },
+];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -3,7 +3,39 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: '#c4a1ff',
+        overlay: 'rgba(0,0,0,0.8)',
+        // background color overlay for alert dialogs, modals, etc.
+        // light mode
+        bg: '#daf5f0',
+        text: '#000',
+        border: '#000',
+        // dark mode
+        darkBg: '#0f3730',
+        darkText: '#eeefe9',
+        darkBorder: '#000',
+      },
+      borderRadius: {
+        base: '5px'
+      },
+      boxShadow: {
+        light: '3px 4px 0px 0px #000',
+        dark: '3px 4px 0px 0px #000',
+      },
+      translate: {
+        boxShadowX: '3px',
+        boxShadowY: '4px',
+        reverseBoxShadowX: '-3px',
+        reverseBoxShadowY: '-4px',
+      },
+      fontWeight: {
+        base: '500',
+        heading: '700',
+      },
+    },
   },
+  darkMode: 'media',
   plugins: [],
 } satisfies Config;
